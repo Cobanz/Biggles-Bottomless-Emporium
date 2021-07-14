3c82b57ea01527b9797fb59c31e29e2344f80fc1
Charles Grant Nichols <grant@inkdots.com>
2021-07-13 20:25:46 -0500

    adjusted the alert

--- client/src/componets/Landing/Landing.jsx
+++ client/src/componets/Landing/Landing.jsx
@@ -2,7 +2,8 @@ import React, { Fragment, useState } from "react";
 import { useAlert } from "react-alert";
 import { Link } from 'react-router-dom'
 import MimicGif from '/Users/grant/Development/capstone/DnD-Project/client/src/componets/mimic_i_like.gif'
-
+import useSound from 'use-sound';
+import WelcomeAudio from '/Users/grant/Development/capstone/DnD-Project/client/src/componets/sounds/Welcome_with_sound_effects.wav';
 
 
 
@@ -10,29 +11,37 @@ export function Landing(props) {
 
     const alert = useAlert();
     const [action, setAction] = useState("");
+
+    const Start = () => {
+        const [play] = useSound(WelcomeAudio);
     
+        return   <button onClick={play}>play me</button>
+      }
+
     return (
         <div>
+            <header>
+                <button
+                    onClick={() => {
+                        alert.show("There are play me buttons around that have audio attatched if you you dont want to hear it just skip them!", {
 
-            <button
-                onClick={() => {
-                    alert.show("This is an alert with extra actions!", {
-                        title: "Alert with extra actions!",
-                        actions: [
-                            {
-                                copy: "Do something",
-                                onClick: () => setAction("Actioned!")
-                            }
-                        ]
-                    });
-                }}
-            >
-                Alert with actions
+                            actions: [
+                                {
+                                    copy: "Do something",
+                                    onClick: () => setAction("Actioned!")
+                                }
+                            ]
+                        });
+                    }}
+                >
+                    CLICK ME BEFORE YOU ENTER
       </button>
+            </header>
 
             <Link to="/">
                 <img src={MimicGif} />
             </Link>
+            <div><Start/></div>
 
         </div>
     )
--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from 'react'
+import { render } from 'react-dom'
+import { transitions, positions, Provider as AlertProvider } from 'react-alert'
+import AlertTemplate from 'react-alert-template-basic'
 import App from './App.jsx';
 import reportWebVitals from './reportWebVitals';
 
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const options = {
+  position: positions.MIDDLE
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Root = () => (
+  <AlertProvider template={AlertTemplate} {...options}
+  // {...options}
+  >
+    <App />
+  </AlertProvider>
+)
+
+render(<Root />, document.getElementById('root'))
